cdf90439e22c2293bcbae367dd9ce50384f9c161
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-01 21:52:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,14 +42,14 @@ class Song
 
   def self.alphabetical #why doesnt this method take in an argument? is it because we are not adding anything to the list but are just sorting what we have?
     self.all.sort_by{|s| s.name}
-    binding.pry
+    
   end
 
   def self.new_from_filename(filename)
     part = filename.split(" - ")
     artist_name = part[0]
-    song_name = part[1].slice!".mp3".
-
+    song_name = part[1].slice!".mp3"
+binding.pry
     song = self.new
     song.name = song_name
 

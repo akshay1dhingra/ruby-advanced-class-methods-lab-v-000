0041279786253117b242d0b526cd416f3b250a6e
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-01 21:27:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
-  def self.alphabetical #why doesnt this method take in an argument? 
+  def self.alphabetical #why doesnt this method take in an argument? is it because we are not adding anything to the list but are just sorting what we have? 
     self.all.sort_by{|s| s.name}
   end
 
